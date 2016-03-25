8dca0f3cf2fbb4a541d39c6341be9654825405a4
gorhgorh <gorhgorh@gmail.com>
2016-03-25 22:43:45 +0100

    standard

--- zones.js
+++ zones.js
@@ -1,16 +1,16 @@
-module.exports={
-  DERIVES_LATERALES : {
-    pan:[112,113],
-    tilt:[114,115],
-    color:'red',
-    focus:100,
-    iris:20
+module.exports = {
+  DERIVES_LATERALES: {
+    pan: [112, 113],
+    tilt: [114, 115],
+    color: 'red',
+    focus: 100,
+    iris: 20
   },
-  PIED_DE_MAT:{
-    pan:[180,30],
-    tilt:[80,115],
-    color:'orange',
-    focus:100,
-    iris:20
+  PIED_DE_MAT: {
+    pan: [180, 30],
+    tilt: [80, 115],
+    color: 'orange',
+    focus: 100,
+    iris: 20
   }
-};
+}
